c0f4a2cf0b782437d08df003e2780a8adf235b14
shuyi130806 <68315201+shuyi130806@users.noreply.github.com>
2020-07-22 14:53:45 +0800

    Update ResourceCentre.java

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -158,21 +158,12 @@ public class ResourceCentre {
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
 		// write your code here
-<<<<<<< HEAD
 		
 		for (int i=0; i<chromebookList.size(); i++) {
 			output += String.format("%-10s %-30s %-10s %-10s %-20\n", chromebookList.get(i).getAssetTag(),
 				chromebookList.get(i).getDescription(),
 				ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
 				chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());	
-=======
-		for (int i = 0; i < chromebookList.size(); i++) {
-
-			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", chromebookList.get(i).getAssetTag(),
-					chromebookList.get(i).getDescription(), 
-					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(),chromebookList.get(i));
->>>>>>> branch 'master' of https://github.com/shuyi130806/Team-1-resource-centre.git
 		}
 		return output;
 	}
@@ -278,7 +269,7 @@ public class ResourceCentre {
 		
 	}
 	
-		//================================= Option 4 Return =================================
+	//================================= Option 4 Return =================================
 	public static boolean doReturnCamcorder(ArrayList<Camcorder> camcorderList,String tag) {
 		boolean isReturned = false;
 
@@ -335,3 +326,6 @@ public class ResourceCentre {
 
 
 	}
+
+
+}
